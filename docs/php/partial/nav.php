58d43afe23856408c194cdecad5d1348f4305cc2
Dhuds1 <dhudson1@northislandcollege.ca>
2023-10-17 16:44:46 -0700

    Change nav if user is logged in or not

--- docs/php/partial/nav.php
+++ docs/php/partial/nav.php
@@ -1,3 +1,7 @@
+<?php
+    $user_logged = false;
+    $user_store = false;
+?>
 <div class="search__bar-wrapper"><input class="search__bar" name="search__bar" type="text" placeholder="Search"><button for="search__bar" class="search__bar-button">Search</button></div>
 <nav class="nav__bar-main">
     <h1>Hello, <?= $user_name ?>!</h1>
@@ -12,15 +16,29 @@
             <li>
                 <a href="">Store</a>
             </li>
-            <div>
+            <?php
+            if($user_logged && $user_store){
+                echo "
+                <div>
                 <li>
-                    <a href="">Manage Store</a>
+                <a href=''>Manage Store</a>
                 </li>
                 <li>
-                    <a href="">Manage Products</a>
+                <a href=''>Manage Products</a>
                 </li>
+                </div>";
+            }
+            else {
+                echo "
+                <div>
+                <li>
+                <a href=''>Create Store</a>
+                </li>
+                </div>
+                ";
+            }
+            ?>
             </div>
-        </div>
         <div>
             <li>
                 <a href="">Profile</a>
