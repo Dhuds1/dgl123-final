7153adba6d6a0aaaaf52eed813e083f66f68abec
Dhuds1 <dhudson1@northislandcollege.ca>
2023-10-17 16:35:05 -0700

    Add basic stlying to the nav bar

--- docs/php/partial/nav.php
+++ docs/php/partial/nav.php
@@ -1,15 +1,33 @@
-Hello <h1><?= $user_name ?></h1>
-<ul>
-    <li>
-        <a href="/">Home</a>
-    </li>
-    <li>
-        <a href="">Wishlist</a>
-    </li>
-    <li>
-        <a href="">Search</a>
-    </li>
-    <li>
-        <a href="">Store</a>
-    </li>
-</ul>
+<div class="search__bar-wrapper"><input class="search__bar" name="search__bar" type="text" placeholder="Search"><button for="search__bar" class="search__bar-button">Search</button></div>
+<nav class="nav__bar-main">
+    <h1>Hello, <?= $user_name ?>!</h1>
+    <ul>
+        <div>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="">Wishlist</a>
+            </li>
+            <li>
+                <a href="">Store</a>
+            </li>
+            <div>
+                <li>
+                    <a href="">Manage Store</a>
+                </li>
+                <li>
+                    <a href="">Manage Products</a>
+                </li>
+            </div>
+        </div>
+        <div>
+            <li>
+                <a href="">Profile</a>
+            </li>
+            <li>
+                <a href="">Settings</a>
+            </li>
+        </div>
+    </ul>
+</nav>
