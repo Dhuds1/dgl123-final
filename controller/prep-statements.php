78436ebd5b1b9038dd735e8450d5aabfe8f523d5
Dhuds1 <dhudson1@northislandcollege.ca>
2023-10-29 11:18:05 -0700

    Add some preped statements. May change later

--- controller/prep-statements.php
+++ controller/prep-statements.php
@@ -2,12 +2,31 @@
 /*
     dummy structure of what I think I want
 */
-// USER
-// user SIGNUP / LOGIN
+
 // user WISHLIST
 // user PROFILE
 // user CART
-
+$prep_statement = [
+    // USERS
+    // USER SIGNUP
+    "signup"=> [
+        "check_username" => "SELECT * FROM cracked_users WHERE user_name = ?;",
+        "check_email"=> "SELECT * FROM cracked_users WHERE user_email = ?",
+        "insert" => "INSERT INTO cracked_users (user_name, user_email, user_password, user_salt, user_firstName, user_lastName, user_DOB, user_gender) VALUES (?, ?, ?, ?, ?, ?, ?, ?);"],
+    // USER LOGIN
+    "login" => [
+        "retrieve" => "SELECT user_password, user_salt FROM cracked_users WHERE user_name = ? OR user_email = ?"
+    ],
+    // USER WISHLIST
+    "wishlist" => [
+        "retrieve" => "SELECT * FROM cracked_wishlist WHERE user_id = ? OR is_public = 1;",
+    ],
+    // USER CART
+    "cart"=> [
+        "retrieve" => "SELECT * FROM cracked_cart WHERE user_id = ?;",
+        "add" => "INSERT INTO cracked_cart (user_id, user_name, store_name, product_name, product_style, product_quantity) VALUES (?, ?, ?, ?, ?, ?);"
+    ],
+];
 // STORE
 // store CREATE
 // store MANAGE
