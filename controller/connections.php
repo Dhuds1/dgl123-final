3d86a58f38618cea19f41410b2058891955e0ced
Dhuds1 <dhudson1@northislandcollege.ca>
2023-10-29 08:03:28 -0700

    Add prep statement layout

---
+++ controller/connections.php
@@ -0,0 +1,11 @@
+<?php
+
+// ACCESSOR
+$accessor = [
+    "users_login"=> [
+        "user"=> "accessor",
+        "host"=> "localhost",
+        "password"=> "passwordUwU:3",//friend came up with this impenetrable password
+        "db" => "cracked_users",
+    ]
+];
---
+++ controller/prep-statements.php
@@ -0,0 +1,24 @@
+<?php
+/*
+    dummy structure of what I think I want
+*/
+// USER
+// user SIGNUP / LOGIN
+// user WISHLIST
+// user PROFILE
+// user CART
+
+// STORE
+// store CREATE
+// store MANAGE
+// store ORDERS
+// store MANAGE PRODUCT
+
+// PRODUCT
+// product SEARCH
+// product POST
+// product DELETE
+// product EDIT
+
+// SETTINGS
+// settings USER
