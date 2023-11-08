4ee995e56d1b3ce85a60ebe8cf8812e7fe5857a1
Dhuds1 <dhudson1@northislandcollege.ca>
2023-11-07 21:23:45 -0800

    Adjusted spacing on product info

--- posts/product.php
+++ posts/product.php
@@ -1,10 +1,22 @@
 <div class="page__content-wrapper">
-    <h1><?= $product['name']?></h1>
-    <div class="product__image">
-        <img src="data:image/jpeg;base64,<?= base64_encode($product['image']) ?>">
+    <div class="">
+        <div class="solo-product__wrapper">
+        <div class="product__image">
+            <img src="data:image/jpeg;base64,<?= base64_encode($product['image']) ?>">
+        </div>
+        <div class="product__info">
+        <h1><?= $product['name']?></h1>
+            <br>
+            Price: <?= $product['price'] ?>
+            <br>
+            Available: <?=  $product['quantity'] === null ? "No Limit" :$product['quantity'] ?>
+            <br>
+            Description: <?= $product['description'] ?>
+            <br>
+            Specifications: <?= $product['specification'] ?>
+            <br>
+            <br>
+            <button>Buy Now</button>
+        </div>
     </div>
-    Price <?= $product['price'] ?>
-    Available <?= $product['quantity'] ?>
-    Description <?= $product['description'] ?>
-    Specifications <?= $product['specification'] ?>
 </div>
